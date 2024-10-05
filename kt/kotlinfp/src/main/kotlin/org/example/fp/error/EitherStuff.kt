de12941ec320f1e82a0dc92f385e093707346b40
nakul bhuwalka <nakulbhuwalka@gmail.com>
2024-10-05 11:27:15 +0530

    error handling

---
+++ kt/kotlinfp/src/main/kotlin/org/example/fp/error/EitherStuff.kt
@@ -0,0 +1,9 @@
+package org.example.org.example.fp.error
+
+fun main() {
+
+}
+
+sealed class Either<out E, out A>
+data class Left<out E>(val value: E) : Either<E, Nothing>()
+data class Right<out A>(val value: A) : Either<Nothing, A>()
